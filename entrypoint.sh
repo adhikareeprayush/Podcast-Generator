712dcbbbaa3b11daafaafa2b7b3950b47a1ad393
Prayush Adhikari <115662438+adhikareeprayush@users.noreply.github.com>
2024-05-16 15:28:52 +0000

    --global

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 
 echo "======================="
 
-git config user.name "${GITHUB_ACTOR}"
-git config user.email "${INPUT_EMAIL}"
+git config --global user.name "${GITHUB_ACTOR}"
+git config --global user.email "${INPUT_EMAIL}"
 git config --global --add safe.directory /github/workspace
 
 python3 /usr/bin/feed.py
